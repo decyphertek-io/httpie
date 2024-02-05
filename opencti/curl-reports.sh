e6542c9e31892d05f1d2dc9e51299da4abc61f5b
decyphertek-io <ciphertek@protonmail.com>
2024-02-05 16:43:43 -0600

    decyphertek-io

--- opencti/curl-reports.sh
+++ opencti/curl-reports.sh
@@ -1,5 +1,5 @@
 curl -X POST  \    
--H "Authorization: Bearer cf993148-854b-4f42-bf58-52da49624020"  \    
+-H "Authorization: Bearer APIKEY"  \    
 -H "Content-Type: application/json"  \   
 -d '{"query": "query { reports { edges { node { id name description } } } }"}'   \   
-"https://opencti.fortifydata.com/graphql"
+"https://IP-OR-Domain/graphql"
